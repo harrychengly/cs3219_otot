e018c17a91aa7dd8ef1464000bae42c392214f9d
harrychengly <harrychengly@gmail.com>
2020-10-04 23:21:26 +0800

    debugging

--- frontendindex.js
+++ frontendindex.js
@@ -1,5 +1,5 @@
 const app = require('./server');
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+// app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
